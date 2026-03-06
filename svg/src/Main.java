8cce3c462c3936483eb620d919faa0d0b0cfca45
Upril <maciekryba321@gmail.com>
2026-03-06 12:05:37 +0100

    lab2 z6

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -8,9 +8,17 @@ public class Main {
         points[4] = new Point(162.4F, 51.3F);
 
         Polygon p1 = new Polygon(points);
-        System.out.println(p1);
-        points[0].setX(0);
-        System.out.println(p1);
+
+        Polygon p2 = new Polygon(new Point[]{
+                new Point(),
+                new Point(10, 0),
+                new Point(10, 10)
+        });
+
+        SvgScene scene = new SvgScene();
+        scene.addPolygon(p1);
+        scene.addPolygon(p2);
+        System.out.println(scene.toSvg());
 
     }
 
