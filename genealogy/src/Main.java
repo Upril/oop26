2a297bb9c99d757cb53033d3bc73f3c46db8cf1c
Upril <maciekryba321@gmail.com>
2026-04-24 11:28:46 +0200

    lab6 zad7

--- genealogy/src/Main.java
+++ genealogy/src/Main.java
@@ -7,9 +7,14 @@ public class Main {
     public static void main(String[] args){
         try {
             List<Person> people = Person.fromCsv("family.csv");
-            System.out.println(people);
+            Person.toBinaryFile(people,"data.bin");
+
+            List<Person> people2 = Person.fromBinaryFile("data.bin");
+            System.out.println(people2);
         } catch (IOException e){
             System.err.println("Błąd dostępu do pliku" + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- genealogy/src/Person.java
+++ genealogy/src/Person.java
@@ -1,11 +1,9 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-public class Person implements Comparable<Person> {
+public class Person implements Comparable<Person>, Serializable {
     private String firstName;
     private String lastName;
     private LocalDate birthday;
@@ -111,6 +109,21 @@ public class Person implements Comparable<Person> {
 
     public String name(){
         return String.format("%s %s",firstName, lastName);
+    }
+
+    public static void toBinaryFile(List<Person> people, String path) throws IOException {
+        FileOutputStream fos = new FileOutputStream(path);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(people);
+        oos.close();
+    }
+
+    public static List<Person> fromBinaryFile(String path) throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(path);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        List<Person> people = (ArrayList<Person>) ois.readObject();
+        ois.close();
+        return people;
     }
 
     @Override
